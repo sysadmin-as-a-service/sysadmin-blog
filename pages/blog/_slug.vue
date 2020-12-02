2d545f7913c2375e5bf1d94b4294c82e4bf47f86
adminmm <matt.mcgrath@onenet.co.nz>
2020-12-02 16:12:00 +1300

    initial commit

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="content">
+  <article>
+    <nuxt-content :document="article"></nuxt-content>
+
+  </article>
+</div>
+</template>
+
+
+
+<script>
+  export default {
+    async asyncData({ $content, params }) {
+      const article = await $content('articles', params.slug).fetch()
+
+      return { article }
+    }
+  }
+</script>

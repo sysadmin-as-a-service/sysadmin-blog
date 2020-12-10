7407757fcc12b38abbfa20700d53333af918eb25
sysadmin-as-a-service <35851895+sysadmin-as-a-service@users.noreply.github.com>
2020-12-10 21:22:46 +1300

    Starting on RecentPosts component

---
+++ components/RecentPosts.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+
+
+    </div>
+</template>
+
+<script>
+export default {
+async asyncData({ $content }) {
+      const articles = await $content('articles',{deep: true}).where({published: {$eq: true}}).sortBy('date', 'desc').fetch()
+
+      return { articles }
+    }
+}
+</script>
+
+<style>
+
+</style>

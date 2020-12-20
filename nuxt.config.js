4395f86c196cddc69bb51fed1023ff0bb5e13c09
sysadmin-as-a-service <35851895+sysadmin-as-a-service@users.noreply.github.com>
2020-12-20 22:46:20 +1300

    Update favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,8 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      { rel: 'alternate icon', type: 'image/x-icon', href: '/favicon.ico' }
     ],
     bodyAttrs: {
       class: 'has-navbar-fixed-top'

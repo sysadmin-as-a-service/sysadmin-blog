2f844636d6f141aca1c07c465801e893e7431a65
sysadmin-as-a-service <35851895+sysadmin-as-a-service@users.noreply.github.com>
2020-12-28 23:06:09 +1300

    Working on typewriter effect

---
+++ components/typewriter.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="typewriter">
+        <span class="title is-size-1">
+            {{ printedText }}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['text'],
+    data() {
+        return {
+            printedText: '',
+            textIndexPos: 0
+        }
+    },
+    methods: {
+        typeText() {
+
+            // each interval, get character at next index and add to printedText
+            if(this.textIndexPos < this.text.length){
+                this.printedText = this.printedText + this.text[this.textIndexPos]
+                this.textIndexPos++
+                setTimeout( () => { this.typeText() }, 500 )
+            }
+        }
+    },
+    mounted() {
+        this.typeText()
+    }
+
+}
+</script>
+
+<style scoped>
+
+.typewriter {
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: .15em solid #4896ca; /* The typwriter cursor */
+  border-radius: 2px;
+  border-width: .1em;
+  white-space: nowrap; /* Keeps the content on a single line */
+  margin: 0 auto; /* Gives that scrolling effect as the typing happens */
+  letter-spacing: .10em; /* Adjust as needed */
+  /* animation: 
+    startAnimation 1s,
+    typing 3s steps(21, end) 0.8s, */
+    /* typing2 0.5s steps(5, end) 2.5s,
+    typing3 2s steps(12, end) 3s, */
+    /* blink-caret .75s step-end infinite; */
+}
+
+/* The typing effect */
+@keyframes startAnimation {
+  from { opacity: 0%; width: 0}
+  to { opacity: 100%; width: 0}
+}
+
+@keyframes typing {
+  from { width: 0 }
+  to { width: 100% }
+}
+
+@keyframes typing2 {
+  from { width: 50% }
+  to { width: 30% }
+}
+
+@keyframes typing3 {
+  from { width: 30% }
+  to { width: 100% }
+}
+
+/* The typewriter cursor effect */
+@keyframes blink-caret {
+  from, to { border-color: transparent }
+  50% { border-color: #017282; }
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,15 @@
           <Logo />
       </div>  
       <div class="column is-half">
-      <div class="typewriter">
+        
+      <!-- <div class="typewriter">
         <span class="title is-size-1">
           sysadmin as a service
         </span>
+      </div> -->
+
+      <typewriter text="sysadmin as a service"></typewriter>
+
       <div class="links">
           <nuxt-link
             :to="$store.state.articles[0].slug"
@@ -35,7 +40,6 @@
             </span>
           </a>
         </div>
-      </div>
       </div>   
 
     </div>  
@@ -82,6 +86,7 @@
 
 <script>
 import tagComponent from "~/components/tagComponent";
+import typewriter from "~/components/typewriter.vue";
 
 export default {  
   async asyncData({ $content, store }) {
@@ -99,7 +104,8 @@ export default {
     }
   },
   components: {
-    tagComponent
+    tagComponent,
+    typewriter
   }
 }
 </script>
@@ -159,49 +165,6 @@ img {
 
 .NuxtLogo {
   max-width: 100%;
-}
-
-.typewriter {
-  overflow: hidden; /* Ensures the content is not revealed until the animation */
-  border-right: .15em solid #4896ca; /* The typwriter cursor */
-  border-radius: 2px;
-  border-width: .1em;
-  white-space: nowrap; /* Keeps the content on a single line */
-  margin: 0 auto; /* Gives that scrolling effect as the typing happens */
-  letter-spacing: .10em; /* Adjust as needed */
-  animation: 
-    startAnimation 1s,
-    typing 3s steps(21, end) 0.8s,
-    /* typing2 0.5s steps(5, end) 2.5s,
-    typing3 2s steps(12, end) 3s, */
-    blink-caret .75s step-end infinite;
-}
-
-/* The typing effect */
-@keyframes startAnimation {
-  from { opacity: 0%; width: 0}
-  to { opacity: 100%; width: 0}
-}
-
-@keyframes typing {
-  from { width: 0 }
-  to { width: 100% }
-}
-
-@keyframes typing2 {
-  from { width: 50% }
-  to { width: 30% }
-}
-
-@keyframes typing3 {
-  from { width: 30% }
-  to { width: 100% }
-}
-
-/* The typewriter cursor effect */
-@keyframes blink-caret {
-  from, to { border-color: transparent }
-  50% { border-color: #017282; }
 }
 
 </style>

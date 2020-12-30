f70f4a100a4f6e511b0cfb4c203565e230408141
sysadmin-as-a-service <35851895+sysadmin-as-a-service@users.noreply.github.com>
2020-12-30 23:06:53 +1300

    Changing blog link on menu to go to homepage

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -15,7 +15,7 @@
       </div>
       <div class="navbar-menu">
         <div class="navbar-start">
-            <a href="/blog" class="navbar-item is-tab">blog</a>
+            <a href="/" class="navbar-item is-tab">blog</a>
             <a href="https://github.com/sysadmin-as-a-service" class="navbar-item is-tab">github</a>
         </div>
         <div class="navbar-end">

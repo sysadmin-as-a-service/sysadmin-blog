8c8e1aa312e3df6b69b86e291c585321abfe9a3b
sysadmin-as-a-service <35851895+sysadmin-as-a-service@users.noreply.github.com>
2021-03-03 21:23:02 +1300

    Generate dynamic routes

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,6 +53,31 @@ export default {
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
   ],
+  generate: {
+    // routes: ['/2020/01/01/access-emails-in-powershell']
+    async routes () {
+      const { $content } = require('@nuxt/content')
+      const files = await $content({ deep: true }).fetch()
+      var routes = [] 
+      for(var i = 0; i < files.length; i++){
+        var file = files[i]
+        if(file.path === '/index'){
+          routes.push("/")
+        }else if(file.date != null){
+          // add an additional route for old wordpress links
+          var d = new Date(file.date)
+          var year = d.getFullYear()
+          // to make it 2 digit and JS months are 0-indexed
+          var month = (d.getMonth()+ 1).toLocaleString(undefined,{minimumIntegerDigits: 2})
+          var day = d.getDate().toLocaleString(undefined,{minimumIntegerDigits: 2})
+          routes.push("/" + year + "/" + month + "/" + day + "/" + file.slug)
+        }
+        
+      }
+      return routes
+      // return files.map(file => file.path === '/index' ? '/' : file.path)
+    }
+  },
 
   // Content module configuration (https://go.nuxtjs.dev/config-content)
   content: {},
